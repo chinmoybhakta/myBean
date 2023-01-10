feb63cc863cc1c418a130258f8564bf6e22929bb
chiniCode <djchinmoy9@gmail.com>
2023-01-11 02:26:34 +0600

    i have commited my files

---
+++ java.js
@@ -0,0 +1,4 @@
+document.getElementById("Test").innerHTML = "Hello World."
+        document.getElementById("Test").style.color = "white"
+        document.getElementById("Test").style.fontSize = "100px"
+        document.getElementById("Test").style.backgroundColor = "red"
